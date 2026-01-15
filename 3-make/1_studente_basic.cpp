65540f752933b62b63af4f85941704fe06e39769
osman-haka <osman.haka05@gmail.com>
2026-01-15 08:11:14 +0100

    Update 1_studente_basic.cpp fatto

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -19,13 +19,19 @@ public:
 
 int main()
 {
-    /*********************** Creazione di oggetti ******************************/
-    // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
-    // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
+    Studente s1;
+    s1.nome = "Mario";
+    s1.eta = 17;
+    s1.media = 7;
 
+    Studente s2;
+    Studente *p = &s2;
+    p->nome = "Maria";
+    p->eta = 18;
+    p->media = 8;
 
-    /************************** Visualizzazione ********************************/
-    // TODO Visualizzare il nome e la media sia di Mario che Maria
+    s1.stampaStudente();
+    s2.stampaStudente();
 
     return 0;
 }
