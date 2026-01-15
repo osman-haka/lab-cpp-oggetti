15b3ae4a6eaaa8d50bbd1fbf269082d757fd5a66
osman-haka <osman.haka05@gmail.com>
2026-01-15 09:22:00 +0100

    Update 2_studente_inc.cpp fatto

--- 3-make/2_studente_inc.cpp
+++ 3-make/2_studente_inc.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 /***********************************************************************************************************
- *                                 Modifica della classe studente (incapsulamento)                         *     
- *   Per come abbiamo definito la classe nel file precedente, è possibile inserire una media di 44:  *         
- *   questo è concettualmente sbagliato, ma il programma non ci darà nessun errore se lo facciamo          *     
- *   Per evitare problemi di questo tipo, si preferisce                                                    *         
- *      1. modificare la "visibilità" dei campi della classe rendendoli privati                            *     
- *      2. creare funzioni che permettono di fare modifiche controllate ai campi,                          * 
- *         (ad esempio in questo caso controllando che la nuova media sia tra 0 e 10)                      * 
+ *                                 Modifica della classe studente (incapsulamento)                         *
+ *   Per come abbiamo definito la classe nel file precedente, è possibile inserire una media di 44:        *
+ *   questo è concettualmente sbagliato, ma il programma non ci darà nessun errore se lo facciamo          *
+ *   Per evitare problemi di questo tipo, si preferisce                                                    *
+ *      1. modificare la "visibilità" dei campi della classe rendendoli privati                            *
+ *      2. creare funzioni che permettono di fare modifiche controllate ai campi,                          *
+ *         (ad esempio in questo caso controllando che la nuova media sia tra 0 e 10)                      *
  *                                                                                                         */
 
 class Studente {
@@ -34,6 +34,17 @@ public:
             media = nuovaMedia;
         else cout << "Impossibile definire media perchè non compresa tra 0 e 10";
     }
+      string getNome() {
+        return nome;
+    }
+
+    int getEta() {
+        return eta;
+    }
+
+    float getMedia() {
+        return media;
+    }
 
     void stampaStudente(){
         cout << "Nome: " << nome << ", eta: " << eta << ", media: " << media << endl;
@@ -42,10 +53,18 @@ public:
 
 int main()
 {
-    // TODO Istanziare un nuovo oggetto e valorizzarlo attraverso i nuovi metodi set
+Studente s1;
 
-    // TODO Adesso che gli attributi della classe sono privati, com'è possibile accedervi? 
-    //      Risolvere il problema creando nuove funzioni "get" che permettano l'accesso a tutti gli attributi della classe
+
+s1.setNome("marco");
+s1.setEta(17);
+s1.setMedia(9);
+
+cout << "nome: " << s1.getNome() << endl;
+cout << "eta: " << s1.getEta() << endl;
+cout << "media: " << s1.getMedia() << endl;
+
 
     return 0;
 }
+
